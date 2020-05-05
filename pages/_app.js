998be99ebd4bf4d6a94af10ebd0b2c4e735a3084
Yorrick Bakker <y.bakker@me.com>
2020-05-05 12:56:45 +0200

    Add basic file structure

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../css/index.scss';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <title>Yorrick Bakker | Web Developer</title>
+      </Head>
+    </>
+  );
+};
+
+export default index;

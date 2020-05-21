93e8ecce6814e11e8339025b365406c8149a2c13
Yorrick Bakker <y.bakker@me.com>
2020-05-21 17:36:59 +0200

    Add links to mast and add next config

--- components/MastIcons.js
+++ components/MastIcons.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import styles from '../css/MastIcons.module.sass'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaFacebook, FaTwitter } from 'react-icons/fa'
 
 const MastIcons = () => {
   return (
     <div className={styles['icon-container']}>
-      <FaGithub />
-      <FaLinkedin />
+      <a
+        href="https://www.github.com/ysbakker"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/yorrick-bakker-203938173/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin />
+      </a>
     </div>
   )
 }

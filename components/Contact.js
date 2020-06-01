2805c82641494ffba519795cc964ba4c4202c8fc
Yorrick Bakker <y.bakker@me.com>
2020-06-01 17:50:20 +0200

    Expand contact component

--- components/Contact.js
+++ components/Contact.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import '../css/Contact.module.sass'
+import Slanted from './Slanted'
 
 const Contact = () => {
-  return null
+  return (
+    <>
+      <Slanted>
+        <div className="heading darkblue">
+          <h1>Contact</h1>
+        </div>
+      </Slanted>
+      <Slanted>
+        <div className="paragraph blue no-margin">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            voluptates ullam architecto praesentium error distinctio sed
+            assumenda nostrum beatae optio modi deserunt dolorem illum eligendi
+            placeat corrupti accusamus, ut repudiandae.
+          </p>
+        </div>
+      </Slanted>
+    </>
+  )
 }
 
 export default Contact

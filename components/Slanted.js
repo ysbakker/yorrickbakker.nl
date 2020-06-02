49afe9b53c6a2cd0a3bfe737b87100abbefa7151
Yorrick Bakker <y.bakker@me.com>
2020-06-02 23:20:07 +0200

    Fix exception for empty className in Slanted

--- components/Slanted.js
+++ components/Slanted.js
@@ -3,15 +3,14 @@ import styles from '../css/Slanted.module.sass'
 
 const Slanted = ({ children, className }) => {
   className =
-    className &&
-    className
-      .split(' ')
-      .reduce((acc, value) => (acc += ` ${styles[value] || value}`), '')
+    (className &&
+      className
+        .split(' ')
+        .reduce((acc, value) => (acc += ` ${styles[value] || value}`), '')) ||
+    ''
 
   return (
-    <div className={`${styles['slant-container']} ${className}`}>
-      {children}
-    </div>
+    <div className={`${styles['slant-container']}${className}`}>{children}</div>
   )
 }
 

5ae38bc9a033ebdd54e902f90068112141fed32b
Yorrick Bakker <y.bakker@me.com>
2020-06-18 22:56:02 +0200

    Add redux devtools extension

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import '../css/colors.sass'
 import '../css/index.sass'
 import '../css/layout.sass'
 
+import { Provider } from 'react-redux'
+import store from '../redux/store'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/root'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store

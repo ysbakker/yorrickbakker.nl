16a8487a56d50517fa3e719dbc0a1a6993cef89f
Yorrick Bakker <y.bakker@me.com>
2020-06-19 18:23:45 +0200

    Add redux thunk

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/root'
 
 const store =
   process.env.NODE_ENV !== 'production'
-    ? createStore(rootReducer, composeWithDevTools())
-    : createStore(rootReducer)
+    ? createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+    : createStore(rootReducer, applyMiddleware(thunk))
 
 export default store

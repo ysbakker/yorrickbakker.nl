3a87c5e47210eaf5305039f2f070d4dce896dfd3
Yorrick Bakker <y.bakker@me.com>
2020-06-19 18:07:39 +0200

    Remove redux devtools from production

--- redux/store.js
+++ redux/store.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/root'
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store =
+  process.env.NODE_ENV !== 'production'
+    ? createStore(rootReducer, composeWithDevTools())
+    : createStore(rootReducer)
 
 export default store

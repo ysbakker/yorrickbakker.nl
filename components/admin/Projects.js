43b4b604863517e6160a4a0f1ffc40bdc0e582a6
Yorrick Bakker <y.bakker@me.com>
2020-06-23 16:53:00 +0200

    Add card contents and 'edit' and 'delete' actions

--- components/admin/Projects.js
+++ components/admin/Projects.js
@@ -3,6 +3,8 @@ import styles from '../../css/Projects.module.sass'
 import { fetchProjects } from '../../redux/projects'
 import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 import { Card } from 'antd'
+import { FaGithub, FaDesktop } from 'react-icons/fa'
+import { AiOutlineEdit, AiFillDelete } from 'react-icons/ai'
 
 const Projects = () => {
   const dispatch = useDispatch()
@@ -12,11 +14,49 @@ const Projects = () => {
     dispatch(fetchProjects())
   }, [])
 
+  const editProject = id => {}
+
+  const deleteProject = id => {}
+
   return (
     <div className={styles['projects-editor']}>
       {data.map(({ _id, heading, text, technologies, codeLink, demoLink }) => (
-        <Card key={_id} title={heading}>
-          {text}
+        <Card
+          key={_id}
+          title={heading}
+          actions={[
+            <AiOutlineEdit
+              className="anticon"
+              key="edit"
+              onClick={() => editProject(_id)}
+            />,
+            <AiFillDelete
+              className="anticon"
+              key="delete"
+              onClick={() => deleteProject(_id)}
+            />,
+          ]}
+        >
+          <p>{text}</p>
+          <p className={styles['project-link']}>
+            <FaGithub />
+            &nbsp;
+            <a href={codeLink} target="_blank" rel="noopener noreferrer">
+              {codeLink}
+            </a>
+          </p>
+          <p className={styles['project-link']}>
+            <FaDesktop />
+            &nbsp;
+            <a href={demoLink} target="_blank" rel="noopener noreferrer">
+              {demoLink}
+            </a>
+          </p>
+          {technologies.map(tech => (
+            <span key={tech} className={styles['tech-highlight']}>
+              {tech}
+            </span>
+          ))}
         </Card>
       ))}
     </div>

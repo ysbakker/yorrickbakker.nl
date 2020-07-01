5fe324fc54c8faae9193107d1468a596a776254d
Yorrick Bakker <y.bakker@me.com>
2020-07-01 18:16:45 +0200

    Add POST route for messages

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -2,7 +2,18 @@ const messages = require('express').Router()
 const messagesModel = require('../models/messages')
 
 messages.get('/', async (req, res) => {
-  res.send('Get messages')
+  const messages = await messagesModel.find()
+  res.send(messages)
+})
+
+messages.post('/', async (req, res) => {
+  const message = req.body
+  try {
+    await messagesModel.create(message)
+  } catch (e) {
+    return res.status(500).send({ message: e.message })
+  }
+  return res.status(201).send()
 })
 
 module.exports = messages

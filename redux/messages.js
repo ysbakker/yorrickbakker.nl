d82ae86599bcc4b998c7dfe097d58c13845c5bdd
Yorrick Bakker <y.bakker@me.com>
2020-07-01 18:31:05 +0200

    Add action creators for fetching and sending messages

--- redux/messages.js
+++ redux/messages.js
@@ -1,11 +1,35 @@
 import produce from 'immer'
 
-const messages = produce((draft, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case '':
-      break
+const TOGGLE_FETCHING = 'Toggle fetching messages'
+
+export const sendMessage = message => {
+  return async dispatch => {
+    dispatch(toggleFetching(true))
   }
-}, {})
+}
+
+const toggleFetching = isFetching => ({
+  type: TOGGLE_FETCHING,
+  payload: isFetching,
+})
+
+export const fetchMessages = () => {
+  return async dispatch => {}
+}
+
+const messages = produce(
+  (draft, action) => {
+    const { type, payload } = action
+    switch (type) {
+      case TOGGLE_FETCHING:
+        draft.fetching = payload || !draft.fetching
+        break
+    }
+  },
+  {
+    fetching: false,
+    data: [],
+  }
+)
 
 export default messages

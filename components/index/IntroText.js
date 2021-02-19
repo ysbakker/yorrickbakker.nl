847dae99a5c4524805ed17c7b3675c8dff48c093
Yorrick Bakker <y.bakker@me.com>
2021-02-19 16:29:24 +0100

    Update intro text

--- components/index/IntroText.js
+++ components/index/IntroText.js
@@ -7,10 +7,12 @@ const IntroText = () => {
       <div className="purple">
         <p>
           Ik ben Yorrick, {age('1999-03-03')} jaar oud en derdejaars student web
-          development aan de HAN in Arnhem. Op het moment loop ik stage bij de
-          Belastingdienst in Apeldoorn bij het Mobile Competence Center. Ik hoop
-          hier mijn kennis op het gebied van .NET en projectontwikkeling uit te
-          breiden.
+          development aan de HAN in Arnhem. Momenteel ben ik bezig met mijn 
+          verdiepende semester, hier breid ik mijn kennis op het gebied van C# en
+          .NET verder uit. Gelijktijdig werk ik aan een project waar ik onderzoek
+          doe naar het gebruik van artificial intellingence bij het begeleiden van
+          studenten in hun propedeuse. Het uiteindelijke doel is om succesfactoren
+          en knelpunten te herkennen.
         </p>
       </div>
     </Slanted>

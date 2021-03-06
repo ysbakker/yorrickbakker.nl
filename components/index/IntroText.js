ae084ebd6fd20edf247839cefe60594d7f00a6c7
Yorrick Bakker <y.bakker@me.com>
2021-03-06 13:54:22 +0100

    Fix typo

--- components/index/IntroText.js
+++ components/index/IntroText.js
@@ -6,13 +6,7 @@ const IntroText = () => {
     <Slanted className="overlap">
       <div className="purple">
         <p>
-          Ik ben Yorrick, {age('1999-03-03')} jaar oud en derdejaars student web
-          development aan de HAN in Arnhem. Momenteel ben ik bezig met mijn 
-          verdiepende semester, hier breid ik mijn kennis op het gebied van C# en
-          .NET verder uit. Gelijktijdig werk ik aan een project waar ik onderzoek
-          doe naar het gebruik van artificial intellingence bij het begeleiden van
-          studenten in hun propedeuse. Het uiteindelijke doel is om succesfactoren
-          en knelpunten te herkennen.
+          Ik ben Yorrick, {age('1999-03-03')} jaar oud en derdejaars student web development aan de HAN in Arnhem. Momenteel ben ik bezig met mijn verdiepende semester, hier breid ik mijn kennis op het gebied van C# en .NET verder uit. Gelijktijdig werk ik aan een project waar ik onderzoek doe naar het gebruik van artificial intelligence bij het begeleiden van studenten in hun propedeuse. Het uiteindelijke doel is om succesfactoren en knelpunten te herkennen.
         </p>
       </div>
     </Slanted>

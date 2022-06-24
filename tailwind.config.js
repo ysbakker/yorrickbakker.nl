3533e3d0a996e17375adb3b520dd32e659f1ec9c
Yorrick Bakker <y.bakker@me.com>
2022-06-24 15:12:08 +0200

    Add base site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   content: ['src/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['Merriweather', ...defaultTheme.fontFamily.serif],
+        'mono': ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+      },
+    }
   },
   plugins: [],
 }
